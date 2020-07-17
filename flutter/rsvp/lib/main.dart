8dbc0091778fb5f6ca5577a02f375a005bac9604
Sam Borhan <sborhan@seattleu.edu>
2020-07-17 11:52:47 -0700

    testing commmit

--- flutter/rsvp/lib/main.dart
+++ flutter/rsvp/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
- // sam is here
+ //sam is here
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
