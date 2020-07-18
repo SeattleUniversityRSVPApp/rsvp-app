2ddd2e2b26aeb16568a27fe41928f933e0f4caae
lmirzaei <41973606+lmirzaei@users.noreply.github.com>
2020-07-18 09:23:17 -0700

    Make fields private and generate getter/setter to access them.

--- flutter/rsvp/lib/event.dart
+++ flutter/rsvp/lib/event.dart
@@ -1,6 +1,18 @@
 class Event {
-  String eventName;
-  String eventDate;
+  String _name;
+  String _date;
 
-  Event({this.eventName, this.eventDate});
+  String get name => _name;
+
+  set name(String value) {
+    _name = value;
+  }
+
+  String get date => _date;
+
+  set date(String value) {
+    _date = value;
+  }
+
+  Event(this._name, this._date);
 }
