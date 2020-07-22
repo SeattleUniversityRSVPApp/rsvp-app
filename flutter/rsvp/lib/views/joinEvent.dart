72ff16038cb41da4415de9aaff7f8482153d08aa
lmirzaei <41973606+lmirzaei@users.noreply.github.com>
2020-07-22 10:36:38 -0700

    Create directories to organize project in a MVVM structure.

---
+++ flutter/rsvp/lib/views/joinEvent.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class JoinEvent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Join in an Event'),
+      ),
+      body: Text('test'),
+    );
+  }
+}
