25f81ab3a2d509cfd76aabd70f40bb7be6f1d3dd
lmirzaei <41973606+lmirzaei@users.noreply.github.com>
2020-07-22 16:06:32 -0700

    Add four events as mock data.

--- flutter/rsvp/lib/models/MockEventService.dart
+++ flutter/rsvp/lib/models/MockEventService.dart
@@ -1,21 +1,34 @@
+import 'dart:collection';
+
 import 'package:rsvp/models/IEventWebServcie.dart';
 
 import 'event.dart';
 
-class MockEventService implements IEventWebService
-{
+class MockEventService implements IEventWebService {
+  Map<String, Event> _mockEventTable = <String, Event>{};
+  int _eventIdCounter = 0;
+
+  MockEventService() {
+    //Add some events:
+    Event _event1;
+    Event _event2;
+    Event _event3;
+    Event _event4;
+    //Add _event1, _event2, _event3, _event4
+  }
+
   @override
   bool createEvent() {
-
+    return false;
   }
 
   @override
   Event getEvent(String link) {
-
+    return null;
   }
 
   @override
   bool editEvent() {
-
+    return false;
   }
 }
