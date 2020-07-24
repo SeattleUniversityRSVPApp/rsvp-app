c551e666684d65c882b5306aa0d17d7c6bc5c9f1
JungBok Cho <chojb2015@gmail.com>
2020-07-23 20:29:06 -0700

    Create Interface and Local Data classes

---
+++ flutter/rsvp/lib/Models/LocalData.dart
@@ -0,0 +1,36 @@
+import 'ILocalData.dart';
+
+class LocalData implements IlocalData {
+
+  String defaultName = null;
+  String userId;
+
+  @override
+  String addCreatedEvents() {
+    return null;
+  }
+
+  @override
+  List<String> getCreatedEvents() {
+    return null;
+  }
+
+  @override
+  String addRespondedEvent() {
+    return null;
+  }
+
+  @override
+  List<String> getRespondedEvents() {
+    return null;
+  }
+
+  List<String> saveEvents() {
+    return null;
+  }
+
+  List<String> loadEvents() {
+    return null;
+  }
+
+}
