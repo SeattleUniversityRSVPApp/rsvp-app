f8ef4763cd588a810484e816a44c98fed6b28d5b
Leila Mirzaei <41973606+lmirzaei@users.noreply.github.com>
2020-07-24 16:26:31 -0700

    Revert "Update Event file in Models"
    This reverts commit 59174f5f1a7155153fc7386952183ad5e59093e9.

--- flutter/rsvp/lib/Models/event.dart
+++ flutter/rsvp/lib/Models/event.dart
@@ -1,58 +1,19 @@
 class Event {
-  String _link;
   String _name;
-  String _date; // Remove it later. Just use _dateTime
-  DateTime _dateTime;
-  String _creator;
-  String _location;
-  bool _status;
+  String _date;
 
-  Event(this._name, this._date); // Remove Later
+  Event(this._name, this._date);
 
-  Event_Constructor(String link, String name, String creator, DateTime dateTime, String location, bool status) {
-    _link = link;
-    _name = name;
-    _creator = creator;
-    _dateTime = dateTime;
-    _location = location;
-    _status = status;
-  }
-
-  String get link => _link;
   String get name => _name;
-  String get creator => _creator;
-  DateTime get dateTime => _dateTime;
-  String get location => _location;
-  bool get status => _status;
-
-  // Remove this later
-  String get date => _date;
-  set date(String value) {
-    _date = value;
-  } // Remove THis later
-
-  set link(String value) {
-    _link = value;
-  }
 
   set name(String value) {
     _name = value;
   }
 
-  set creator(String value) {
-    _creator = value;
-  }
+  String get date => _date;
 
-  set dateTime(DateTime value) {
-    _dateTime = value;
-  }
-
-  set location(String value) {
-    _location = value;
-  }
-
-  set status(bool value) {
-    _status = value;
+  set date(String value) {
+    _date = value;
   }
 
 }
