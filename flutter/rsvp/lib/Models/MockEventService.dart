30b4317be00fa35d75670a2667731b0b0c732ef0
JungBok Cho <chojb2015@gmail.com>
2020-07-24 14:55:28 -0700

    Add more events in MockEventService file

--- flutter/rsvp/lib/Models/MockEventService.dart
+++ flutter/rsvp/lib/Models/MockEventService.dart
@@ -9,15 +9,20 @@ class MockEventService implements IEventWebService {
 
   MockEventService() {
     //Add some events in the mock table:
-    var event1 = Event('Basketball Game', '08/29/2020');
-//    var event2 = Event();
-//    var event3 = Event();
-//    var event4 = Event();
-//    var event5 = Event();
+    var event1 = Event('Basketball Game', '08/03/2020');
+    var event2 = Event('Walk with dogs', '08/16/2020');
+    var event3 = Event('Finish this project', '09/08/2020');
+    var event4 = Event('Visit Museum', '10/28/2020');
+    var event5 = Event('Find internship', '11/17/2020');
+    var event6 = Event('Party Night', '12/31/2020');
 
     _mockEventTable[_urlTemplate + _eventIdCounter.toString()] = event1;
-    _eventIdCounter++;
-//    _mockEventTable[_urlTemplate + _eventIdCounter.toString()] = event2;
+    _mockEventTable[_urlTemplate + _eventIdCounter.toString()] = event2;
+    _mockEventTable[_urlTemplate + _eventIdCounter.toString()] = event3;
+    _mockEventTable[_urlTemplate + _eventIdCounter.toString()] = event4;
+    _mockEventTable[_urlTemplate + _eventIdCounter.toString()] = event5;
+    _mockEventTable[_urlTemplate + _eventIdCounter.toString()] = event6;
+    _eventIdCounter += 6;
   }
 
   @override
