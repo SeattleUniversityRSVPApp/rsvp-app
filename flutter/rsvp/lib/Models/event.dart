a572bd25bea430e30f3dda2ad76801c772803e89
JungBok Cho <chojb2015@gmail.com>
2020-07-24 22:21:49 -0700

    Update event class in Models

--- flutter/rsvp/lib/Models/event.dart
+++ flutter/rsvp/lib/Models/event.dart
@@ -3,27 +3,33 @@ class Event {
   String _name;
   String _date; // Remove it later. Just use _dateTime
   DateTime _dateTime;
+  String _description;
   String _creator;
   String _location;
+  int _minNum;
   bool _status;
 
   Event(this._name, this._date); // Remove Later
 
-  Event_Constructor(String link, String name, String creator, DateTime dateTime, String location, bool status) {
+  Event_Constructor(String link, String name, String creator, String description, String date, String location, int minNum, bool status) {
     _link = link;
     _name = name;
     _creator = creator;
-    _dateTime = dateTime;
+    _description = description;
+    _date = date;
     _location = location;
+    _minNum = minNum;
     _status = status;
   }
 
   String get link => _link;
   String get name => _name;
+  int get minNum => _minNum;
   String get creator => _creator;
   DateTime get dateTime => _dateTime;
   String get location => _location;
   bool get status => _status;
+  String get description => _description;
 
   // Remove this later
   String get date => _date;
@@ -31,12 +37,21 @@ class Event {
     _date = value;
   } // Remove THis later
 
+
+  set description(String value) {
+    _description = value;
+  }
+
   set link(String value) {
     _link = value;
   }
 
   set name(String value) {
     _name = value;
+  }
+
+  set minNum(int value) {
+    _minNum = value;
   }
 
   set creator(String value) {
