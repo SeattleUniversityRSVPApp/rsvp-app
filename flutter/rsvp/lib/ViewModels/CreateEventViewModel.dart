5d1317b5c1b41ee64acad04e7ba6db85fc5e7e9c
JungBok Cho <chojb2015@gmail.com>
2020-07-24 22:32:32 -0700

    Update CreateEventViewModel in ViewModels

---
+++ flutter/rsvp/lib/ViewModels/CreateEventViewModel.dart
@@ -0,0 +1,8 @@
+class CreateEventViewModel {
+  String defaultCreatorName = "Unknown";
+
+  createEventViewModel(String eventName, String eventDate, int minNum, String eventDescription, String eventLocation) {
+
+  }
+
+}
