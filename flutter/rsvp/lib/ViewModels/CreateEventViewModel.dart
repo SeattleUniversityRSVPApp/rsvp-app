7e7aa192da6ec1467d69f4c7bdd5bdb5a097ff8d
lmirzaei <41973606+lmirzaei@users.noreply.github.com>
2020-07-25 17:07:49 -0700

    Reformat code.

--- flutter/rsvp/lib/ViewModels/CreateEventViewModel.dart
+++ flutter/rsvp/lib/ViewModels/CreateEventViewModel.dart
@@ -1,8 +1,6 @@
 class CreateEventViewModel {
   String defaultCreatorName = "Unknown";
 
-  createEvent(String eventName, String eventDate, int minNum, String eventDescription, String eventLocation) {
-
-  }
-
+  createEvent(String eventName, String eventDate, int minNum,
+      String eventDescription, String eventLocation) {}
 }
