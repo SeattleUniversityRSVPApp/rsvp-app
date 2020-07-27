fe665349aa1f17bf4c149a8b764251d667e3dcc0
lmirzaei <41973606+lmirzaei@users.noreply.github.com>
2020-07-27 15:52:33 -0700

    Implement create event method.

--- flutter/rsvp/lib/ViewModels/CreateEventViewModel.dart
+++ flutter/rsvp/lib/ViewModels/CreateEventViewModel.dart
@@ -1,6 +1,18 @@
+import 'package:rsvp/Models/IEventRepository.dart';
+
 class CreateEventViewModel {
   String defaultCreatorName = "Unknown";
+  IEventRepository _eventRepository;
 
-  createEvent(String eventName, String eventDate, int minNum,
-      String eventDescription, String eventLocation) {}
+  CreateEventViewModel(this._eventRepository);
+
+  bool createEvent(String eventName, String eventDate, int minNum,
+      String eventDescription, String eventLocation) {
+    //if the creator name that is passed as an argument is empty then replace the default name as CreatorName.
+    var aNewEvent = _eventRepository.createEvent(
+        eventName, eventDate, minNum, eventDescription, eventLocation);
+    //TODO: check if creation was successful then return true, otherwise return false.
+    var createdSuccessful = true;
+    return createdSuccessful;
+  }
 }
