d75b73aab014a0ed01e137cb8492e91ec4e69439
Sam Borhan <sborhan@seattleu.edu>
2020-07-29 15:08:31 -0700

    adding Setting title to setting-view

--- flutter/rsvp/lib/Views/setting.dart
+++ flutter/rsvp/lib/Views/setting.dart
@@ -1,15 +1,62 @@
+
 import 'package:flutter/material.dart';
 
-class Setting extends StatelessWidget {
+class Setting extends StatefulWidget {
+  @override
+  _SettingState createState() => _SettingState();
+}
+
+class _SettingState extends State<Setting> {
+
+  String name;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[200],
       appBar: AppBar(
         title: Text('Setting'),
         centerTitle: true,
         backgroundColor: Colors.grey[800],
       ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: <Widget>[
+              getName(),
+              RaisedButton(
+                  child: Text('Save'),
+                  onPressed: () async => {
+                    Navigator.pop(context),
+                  })
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Row getName() {
+    return Row(
+      children: <Widget>[
+        Text(' Enter Name: '),
+        Container(
+          width: 200,
+          child: TextField(
+            decoration: InputDecoration.collapsed(hintText: ' type your name '),
+            onChanged: (String text) {
+              setState(() {
+                name = text;
+              });
+            },
+            onSubmitted: (String text) {
+              setState(() {
+                name = text;
+              });
+            },
+          ),
+        ),
+      ],
     );
   }
 }
