02676b22c9e848ef0c665aeadea551b2f14a7f60
Sam Borhan <sborhan@seattleu.edu>
2020-07-29 12:14:28 -0700

    adding Setting title to setting-view

--- flutter/rsvp/lib/Views/setting.dart
+++ flutter/rsvp/lib/Views/setting.dart
@@ -4,13 +4,12 @@ class Setting extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.grey[200],
-
-    appBar: AppBar(
-    title: Text('RSVP'),
-    centerTitle: true,
-    backgroundColor: Colors.grey[800]
-    ),
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('Setting'),
+        centerTitle: true,
+        backgroundColor: Colors.grey[800],
+      ),
     );
   }
 }
