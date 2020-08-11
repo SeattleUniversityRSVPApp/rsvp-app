e9bee551877dfe9a1a7c64d9aaf73b1121fc0884
Ruifeng Wang <rwang@seattleu.edu>
2020-08-11 01:46:13 -0700

    Edit joinEvent method in JoinEventViewModel.dart

--- flutter/rsvp/lib/ViewModels/JoinEventViewModel.dart
+++ flutter/rsvp/lib/ViewModels/JoinEventViewModel.dart
@@ -1,22 +1,18 @@
 import 'package:flutter/foundation.dart';
-import 'package:intl/intl.dart';
-import 'package:rsvp/Models/EventRepository.dart';
 import 'package:rsvp/Models/IEventRepository.dart';
 
 class JoinEventViewModel extends ChangeNotifier{
   IEventRepository _repository;
-  var _joinedSuccess = true;
 
-//  JoinEventViewModel(this._repository);
+  JoinEventViewModel(this._repository);
 
   bool joinEvent(String link, String respondentName) {
-    try {
-      _repository.joinEvent(link, respondentName);
-      _joinedSuccess = true;
-    } catch (exception) {
-      _joinedSuccess = false;
+    var joinedEvent = _repository.joinEvent(link, respondentName);
+    if(joinedEvent == null) {
+      return false;
+    } else {
+      return true;
     }
-    return _joinedSuccess;
   }
 
   String getUsername() {
