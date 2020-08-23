6d4d50c04027b574514da3855ce087c537237568
lmirzaei <41973606+lmirzaei@users.noreply.github.com>
2020-08-22 19:28:04 -0700

    Add a class for the real web service, called `EventWebService` to send http requests.

---
+++ flutter/rsvp/lib/Models/EventWebService.dart
@@ -0,0 +1,34 @@
+import 'package:rsvp/Models/IEventWebServcie.dart';
+import 'package:rsvp/Models/event.dart';
+
+class EventWebService implements IEventWebService {
+  @override
+  Event createEvent(String eventName, String creatorName, DateTime eventDate,
+      int minNum, String eventDescription, String eventLocation){
+    // TODO: implement createEvent
+    return null;
+  }
+
+  @override
+  bool editEvent() {
+    // TODO: implement editEvent
+    return false;
+  }
+
+  @override
+  Event getEvent(String link) {
+    return null;
+  }
+
+  @override
+  bool joinEvent(String link, String name) {
+    // TODO: implement joinEvent
+    return false;
+  }
+
+  @override
+  bool cancelEvent(String link) {
+    // TODO: implement cancelEvent
+    return false;
+  }
+}
